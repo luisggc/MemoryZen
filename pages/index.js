21880996a242b97c10c77d9318fc8b82569d1c88
Luis <luiscoimbraeng@outlook.com>
2022-08-27 15:59:33 -0300

    HeaderFoot from HOC to Regular Component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
-import Head from "next/head";
-import Header from "../components/Header";
 import Front from "../components/Front";
 import ProductCategories from "../components/ProductCategories";
 import ShopSection from "../components/ShopSection";
-import Footer from "../components/Footer";
-import withHeaderFoot from "../components/withHeaderFoot";
+import DefaultLayout from "../components/DefaultLayout";
 
 const Home = () => {
   return (
-    <>
+    <DefaultLayout>
       <Front />
       <ProductCategories />
-    </>
+      <ShopSection />
+    </DefaultLayout>
   );
 };
 
-export default withHeaderFoot(Home);
+export default Home;
